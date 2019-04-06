4e10df59340d0261ba932ebb3984440b5acefa56
kpestov <kpestov91@gmail.com>
2019-04-06 12:47:09 +0500

    rewrite views

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
+
+
 from .models import Post, Tag
 from django.views.generic import View
 
@@ -10,7 +14,8 @@ def posts_list(request):
 
 class PostDetail(View):
     def get(self, request, slug):
-        post = Post.objects.get(slug__iexact=slug)
+        post = get_object_or_404(Post, slug__iexact=slug)
+        # post = Post.objects.get(slug__iexact=slug)
         return render(request, 'blog/post_detail.html', context={'post': post})
 
 
@@ -18,14 +23,20 @@ class PostDetail(View):
 #     post = Post.objects.get(slug__iexact=slug)
 #     return render(request, 'blog/post_detail.html', context={'post': post})
 
-
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(View):
+    def get(self, request, slug):
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        # tag = Tag.objects.get(slug__iexact=slug)
+        return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+# def tag_detail(request, slug):
+#     tag = Tag.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
 
