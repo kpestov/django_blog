7fc748c5ff390df48507db5dcfdc8e72639f4996
kpestov <kpestov91@gmail.com>
2019-04-06 20:36:48 +0500

    added form for post creation

--- blogengine/blog/forms.py
+++ blogengine/blog/forms.py
@@ -1,4 +1,4 @@
-from .models import Tag
+from .models import Tag, Post
 from django import forms
 
 from django.core.exceptions import ValidationError
@@ -21,10 +21,17 @@ class TagForm(forms.ModelForm):
         }
 
     def clean_slug(self):
+        """
+        it lowers the slug. E.g. Django -> django
+        :return:
+        """
+
+        # This is an anti-pattern but here it is 100% that slug exists, so instead 'self.cleaned_data.get('slug').lower'
+        # we can use lower expression.
         new_slug = self.cleaned_data['slug'].lower()
 
         if new_slug == 'create':
-            raise ValidationError('Slug can not be create!')
+            raise ValidationError('Slug can not be created!')
         if Tag.objects.filter(slug__iexact=new_slug).count():
             raise ValidationError('Slug must be unique! We have "{}" slug already '.format(new_slug))
 
@@ -36,3 +43,25 @@ class TagForm(forms.ModelForm):
     #                   slug=self.cleaned_data['slug']
     #                   )
     #     return new_tag
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Post
+        fields = ['title', 'slug', 'body', 'tags']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control'})
+        }
+
+    def clean_slug(self):
+
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug can not be created!')
