d42bc491347477103f778725b8538cc0c0d61544
kpestov <kpestov91@gmail.com>
2019-04-06 15:38:48 +0500

    created tag form

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 
 from .models import Post, Tag
 from django.views.generic import View
@@ -39,6 +40,14 @@ class TagCreate(View):
         form = TagForm()
         return render(request, 'blog/tag_create.html', context={'form': form})
 
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
 
 
 # class TagDetail(View):
