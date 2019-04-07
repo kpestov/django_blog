d4917cd11a6d0cdb3320765e8a2f2f31040f76bc
kpestov <kpestov91@gmail.com>
2019-04-07 13:05:02 +0500

    added post delete

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -22,6 +22,9 @@ class Post(models.Model):
     def get_update_url(self):
         return reverse('post_update_url', kwargs={'slug': self.slug})
 
+    def get_delete_url(self):
+        return reverse('post_delete_url', kwargs={'slug': self.slug})
+
     def save(self, *args, **kwargs):
         """
         Override method save() of parent method for auto generation of slug.
@@ -47,6 +50,9 @@ class Tag(models.Model):
 
     def get_update_url(self):
         return reverse('tag_update_url', kwargs={'slug': self.slug})
+
+    def get_delete_url(self):
+        return reverse('tag_delete_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return self.title
--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -5,13 +5,63 @@ from django.urls import reverse
 
 from .models import Post, Tag
 from django.views.generic import View
-from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin
+from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
 from .forms import TagForm, PostForm
 
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create.html'
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update.html'
+
+
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete.html'
+    redirect_url = 'posts_list_url'
+
+
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update.html'
+
+
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete.html'
+    redirect_url = 'tags_list_url'
+
 
 
 # class PostDetail(View):
@@ -21,41 +71,15 @@ def posts_list(request):
 #         return render(request, 'blog/post_detail.html', context={'post': post})
 
 
-class PostDetail(ObjectDetailMixin, View):
-    model = Post
-    template = 'blog/post_detail.html'
-
-
-def tags_list(request):
-    tags = Tag.objects.all()
-    return render(request, 'blog/tags_list.html', context={'tags': tags})
-
-
-class TagDetail(ObjectDetailMixin, View):
-    model = Tag
-    template = 'blog/tag_detail.html'
-
-
-class TagCreate(ObjectCreateMixin, View):
-    model_form = TagForm
-    template = 'blog/tag_create.html'
-
-
-class TagUpdate(ObjectUpdateMixin, View):
-    model = Tag
-    model_form = TagForm
-    template = 'blog/tag_update.html'
-
-
-class TagDelete(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        return render(request, 'blog/tag_delete.html', context={'tag': tag})
-
-    def post(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        tag.delete()
-        return redirect(reverse('tags_list_url'))
+# class TagDelete(View):
+#     def get(self, request, slug):
+#         tag = Tag.objects.get(slug__iexact=slug)
+#         return render(request, 'blog/tag_delete.html', context={'tag': tag})
+#
+#     def post(self, request, slug):
+#         tag = Tag.objects.get(slug__iexact=slug)
+#         tag.delete()
+#         return redirect(reverse('tags_list_url'))
 
     # def get(self, request, slug):
     #     tag = Tag.objects.get(slug__iexact=slug)
@@ -72,10 +96,7 @@ class TagDelete(View):
     #     return render(request, 'blog/tag_update.html', context={'tag': tag, 'form': bound_form})
 
 
-class PostUpdate(ObjectUpdateMixin, View):
-    model = Post
-    model_form = PostForm
-    template = 'blog/post_update.html'
+
 
 
     # def get(self, request):
@@ -92,9 +113,7 @@ class PostUpdate(ObjectUpdateMixin, View):
     #     return render(request, 'blog/tag_create.html', context={'form': bound_form})
 
 
-class PostCreate(ObjectCreateMixin, View):
-    model_form = PostForm
-    template = 'blog/post_create.html'
+
 
     # def get(self, request):
     #     form = PostForm()
